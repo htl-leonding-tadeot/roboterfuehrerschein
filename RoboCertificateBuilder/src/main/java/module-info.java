3016997b882a7d504e90b07a0dd48d6b1e1f00b3
aichingert <aichinger.tobias13@gmail.com>
2024-01-19 16:00:51 +0100

    feat: init new robo builder

---
+++ RoboCertificateBuilder/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module at.htl.leonding.robocertificatebuilder {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens at.htl.leonding.robocertificatebuilder to javafx.fxml;
+    exports at.htl.leonding.robocertificatebuilder;
+}
