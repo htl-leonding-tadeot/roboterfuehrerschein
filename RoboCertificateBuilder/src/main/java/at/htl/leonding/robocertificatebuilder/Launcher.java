28134a03d9b6fa2dabfe432453cdbb725f63bbb6
ICQun <jln.schludermann@gmail.com>
2024-01-19 22:23:17 +0100

    feat: add shade for Certificate builder

---
+++ RoboCertificateBuilder/src/main/java/at/htl/leonding/robocertificatebuilder/Launcher.java
@@ -0,0 +1,8 @@
+package at.htl.leonding.robocertificatebuilder;
+
+// Wrapper class needed for fat-jars to work
+public class Launcher {
+    public static void main(String[] args) {
+        RoboCertificateApplication.main(args);
+    }
+}
